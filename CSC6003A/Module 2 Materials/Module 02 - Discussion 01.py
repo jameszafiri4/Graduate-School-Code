64eacde72f4dec8d60e8cf5206e641a2692ee4bf
James Zafiri <139516505+jameszafiri4@users.noreply.github.com>
2025-02-15 11:38:04 -0500

    Adding files

---
+++ CSC6003A/Module 2 Materials/Module 02 - Discussion 01.py
@@ -0,0 +1,9 @@
+for i in range(50, 24, -5):
+    print(i)
+    
+leave = "EXIT"
+
+while True:
+    word = input("Enter a word (Type 'EXIT' to stop the program): ")
+    if word == leave:
+        break
