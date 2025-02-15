62b35a420eeb5528af6f4518b9fb1615ea8f4c95
James Zafiri <139516505+jameszafiri4@users.noreply.github.com>
2025-02-15 11:41:18 -0500

    Delete Module 3 Materials directory

--- Module 3 Materials/Slide13.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-print("The name of your program is " + sys.argv[0])
-print("The number of command line arguements is ", len(sys.argv)-1)
-argcount = len(sys.argv)-1
-
-while (argcount >= 1):
-    print("The " + str(argcount) + "st arg is " + sys.argv[argcount])
-    argcount -= 1
